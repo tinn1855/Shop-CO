22837cda7ae4e742ea4239df0a53e063f7492016
tinnguyen <tinn1855@gmail.com>
2025-02-28 11:27:23 +0700

    build prod

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
-import path from "path";
 
 export default defineConfig({
   root: __dirname,
@@ -34,7 +33,6 @@ export default defineConfig({
       transformMixedEsModules: true,
     },
   },
-  base:'/',
   test: {
     watch: false,
     globals: true,

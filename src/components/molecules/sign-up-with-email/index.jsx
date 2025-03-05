ce8633493f2d697673b86848f3d3458c2e2c84ba
tinnguyen <tinn1855@gmail.com>
2025-03-05 15:38:49 +0700

    add feature create an account

--- src/components/molecules/sign-up-with-email/index.jsx
+++ src/components/molecules/sign-up-with-email/index.jsx
@@ -1,23 +1,90 @@
+import { useForm } from 'react-hook-form';
 import { Button } from '../../ui/button';
-import { Checkbox } from '../../ui/checkbox';
 import { Input } from '../../ui/input';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '../../ui/form';
+import { useCreateUser } from '@/src/hooks/queries/use-create-user';
+import { Toaster } from '../../ui/sonner';
+
+const formSignUp = z.object({
+  name: z.string().min(2).max(20),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
 
 export function SignUpWithEmail() {
+  const { createUser, data, error, loading } = useCreateUser();
+  const form = useForm({
+    resolver: zodResolver(formSignUp),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmitFormSignUp(values) {
+    createUser(values);
+    console.log(values);
+  }
+
   return (
-    <form action="" className="lg:w-3/5 w-full flex flex-col gap-5">
-      <div className="flex flex-col gap-5">
-        <Input placeholder="Full name" className="w-full" />
-        <Input placeholder="Email" className="w-full" />
-        <Input placeholder="Password" type="password" />
-        <Input placeholder="Re-enter password" type="password" />
-        <div className="flex items-center">
-          <Checkbox />
-          <label className="ml-2">I agree to the terms</label>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmitFormSignUp)}
+        className="lg:w-3/5 w-full flex flex-col gap-5"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Full Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div>
+          <Button type="submit" disabled={loading}>
+            {loading ? 'Submitting...' : 'Submit'}
+          </Button>
+          {error && <Toaster richColors />}
+          {data && <Toaster richColors />}
         </div>
-      </div>
-      <div>
-        <Button>Sign Up</Button>
-      </div>
-    </form>
+      </form>
+    </Form>
   );
 }
---
+++ src/hooks/queries/use-create-user.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { toast } from 'sonner';
+
+export function useCreateUser() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const createUser = async (userData) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch('https://api.escuelajs.co/api/v1/users/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          ...userData,
+          avatar: 'https://picsum.photos/800',
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(toast.error('Create an account fail'));
+      }
+      const result = await response.json();
+      setData(result);
+      toast.success('Create an account successfully');
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { createUser, data, error, loading };
+}
